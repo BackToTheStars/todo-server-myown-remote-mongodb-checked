17c3aec7a3348f7978dd286409367f2ec0c2dba8
BackToTheStars <nikgrigoryev@gmail.com>
2020-08-22 15:05:09 -0700

    checked - all works

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -4,11 +4,13 @@ export default function mongoConnection() {
   mongoose.set('useCreateIndex', true);
 
   mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${
-      process.env.MONGO_HOST
-    }/${process.env.MONGO_DB_NAME}`,
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
     { useNewUrlParser: true, useUnifiedTopology: true },
   );
+
+  //  All environment variables were saved in .env
+  //  then added to .gitignore
+  //  then added to heroku environment variables
 
   mongoose.connection.on('error', () => {
     throw new Error('Unable to connect to database');
